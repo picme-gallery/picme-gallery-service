225a8767ee9e82795ca67cf2d24a6b6bfbc8370e
Shayan <sgolafshani@cnm.edu>
2020-10-22 13:29:50 -0600

    Initial commit... set up project

---
+++ src/main/java/edu/cnm/deepdive/picmegallery/PicmeGalleryApplication.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.picmegallery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicmeGalleryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PicmeGalleryApplication.class, args);
+  }
+
+}
---
+++ src/test/java/edu/cnm/deepdive/picmegallery/PicmeGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.cnm.deepdive.picmegallery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicmeGalleryApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
