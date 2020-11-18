82726c7a0abba94be1d309eca753286438b8ed97
Alex Garber <alexgarber6@gmail.com>
2020-11-17 20:23:25 -0700

    Added to Event controller and event service

--- src/main/java/edu/cnm/deepdive/picmegallery/controller/EventController.java
+++ src/main/java/edu/cnm/deepdive/picmegallery/controller/EventController.java
@@ -3,10 +3,13 @@ package edu.cnm.deepdive.picmegallery.controller;
 import edu.cnm.deepdive.picmegallery.model.entity.Event;
 import edu.cnm.deepdive.picmegallery.model.entity.User;
 import edu.cnm.deepdive.picmegallery.service.EventService;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.server.ExposesResourceFor;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +28,13 @@ public class EventController {
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-  public Event post(@RequestBody Event event, Authentication auth, String passkey) {
-    return eventService.save(event,(User) auth.getPrincipal(), passkey);
+  public Event post(@RequestBody Event event, Authentication auth, String passkey, String name) {
+    return eventService.save(event,(User) auth.getPrincipal(), passkey,name);
   }
 
+  @GetMapping(value = {"/{id}/{passkey}"}, produces = MediaType.APPLICATION_JSON_VALUE)
+  public Optional<Event> get(@PathVariable Long id, @PathVariable String passkey){
+    return eventService.get(id, passkey);
+  }
 
 }
--- src/main/java/edu/cnm/deepdive/picmegallery/service/EventService.java
+++ src/main/java/edu/cnm/deepdive/picmegallery/service/EventService.java
@@ -3,6 +3,8 @@ package edu.cnm.deepdive.picmegallery.service;
 import edu.cnm.deepdive.picmegallery.model.dao.EventRepository;
 import edu.cnm.deepdive.picmegallery.model.entity.Event;
 import edu.cnm.deepdive.picmegallery.model.entity.User;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,14 +18,19 @@ public class EventService {
     this.eventRepository = eventRepository;
   }
 
- public Event save(Event event, User user, String passkey) {
+ public Event save(Event event, User user, String passkey, String name) {
     if (event.getId() == null || event.getId() == 0){
       event.setUser(user);
-      //TODO Need to implement a passkey
-      //TODO Ask Todd
       event.setPasskey(passkey);
+      event.setName(name);
     }
     return eventRepository.save(event);
+ }
+
+ public Optional<Event> get(Long id, String passkey){
+    return eventRepository.findByIdAndPasskey(id, passkey);
+
+
  }
 
 
