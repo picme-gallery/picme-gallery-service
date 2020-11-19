be682ace9812cb93e281d5d56ab37decfb54a5bf
Alex Garber <alexgarber6@gmail.com>
2020-11-19 13:24:19 -0700

    Java Docs DONE !!!!! :)

--- src/main/java/edu/cnm/deepdive/picmegallery/PicmeGalleryApplication.java
+++ src/main/java/edu/cnm/deepdive/picmegallery/PicmeGalleryApplication.java
@@ -5,10 +5,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType;
 
+/**
+ * This class has the main method which starts the spring boot application.
+ */
 @EnableHypermediaSupport(type = {HypermediaType.HAL})
 @SpringBootApplication
 public class PicmeGalleryApplication {
 
+  /**
+   * This method is the main method that starts the application.
+   * @param args is the command line arguments we pass in to the main method.
+   */
   public static void main(String[] args) {
     SpringApplication.run(PicmeGalleryApplication.class, args);
   }
